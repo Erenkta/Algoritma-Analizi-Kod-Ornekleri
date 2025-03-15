ef280df3f8c382f03357688f820a68e3b48a121a
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-03-15 19:08:22 +0300

    Algoritma analizi hafta 2 kod örnekleri ( Böl ve fethet )

---
+++ Algoritma Analizi/Hafta_2/MaxSubArraySum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Analizi.Hafta_2
+{
+    public class MaxSubArraySum
+    {
+
+        public int MaxSubarraySum(int[] arr)
+        {
+            return Helper(arr, 0, arr.Length - 1);
+        }
+        public int Helper(int[] arr, int low, int high)
+        {
+            if (low == high)
+                return arr[low];
+
+            int mid = (low + high) / 2;
+
+            // Sol, sağ ve ortadan geçen maksimumları bul
+            int leftMax = Helper(arr, low, mid);
+            int rightMax = Helper(arr, mid + 1, high);
+            int crossMax = MaxCrossingSum(arr, low, mid, high);
+
+            return Math.Max(leftMax, Math.Max(rightMax, crossMax));
+        }
+        public int MaxCrossingSum(int[] arr, int low, int mid, int high)
+        {
+            // Orta noktadan sola doğru maksimum toplam
+            int leftSum = int.MinValue;
+            int total = 0;
+            for (int i = mid; i >= low; i--)
+            {
+                total += arr[i];
+                if (total > leftSum)
+                    leftSum = total;
+            }
+
+            // Orta noktadan sağa doğru maksimum toplam
+            int rightSum = int.MinValue;
+            total = 0;
+            for (int i = mid + 1; i <= high; i++)
+            {
+                total += arr[i];
+                if (total > rightSum)
+                    rightSum = total;
+            }
+
+            return leftSum + rightSum;
+        }
+    }
+}
--- Algoritma Analizi/Program.cs
+++ Algoritma Analizi/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Algoritma_Analizi;
 using Algoritma_Analizi.Hafta_1;
+using Algoritma_Analizi.Hafta_2;
+using System.Runtime.InteropServices;
 
 
 Console.WriteLine("Algoritma Analizi Dersi Kod Örnekleri : ");
@@ -41,4 +43,19 @@ Console.WriteLine("  \n Zaman karmaşıklığı : \n " +
 
 Console.WriteLine("----------------------------------------------------------------- \n");
 
+MaxSubArraySum subArraySum = new MaxSubArraySum();
+int[] subArray = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+int maxSum = subArraySum.MaxSubarraySum(subArray);
+
+Console.WriteLine($"Maksimum Alt Dizi Toplamı: {maxSum} ");
+
+Console.WriteLine("  \n Zaman karmaşıklığı : \n " +
+    "Omega ( En iyi durum )   : n log(n) \n " +
+    "Theta ( Ortalama durum ) : n log(n) \n " +
+    "Big-O ( En kötü durum )  : n log(n) \n");
+
+
+
+
+Console.WriteLine("----------------------------------------------------------------- \n");
 
